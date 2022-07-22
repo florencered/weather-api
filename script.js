5221ff01175cec2e5c77f00860c81266ba776ba3
Dhriti Dey <92095533+florencered@users.noreply.github.com>
2022-07-23 02:19:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,70 @@
-console.log("working"); 
-let weather={ //storing req API Creds
-	//api key to access the weather,store the req creds as objects
-	"apikey": "f06efb9fc4cc400992a24f1a65a6aa02",
-	//func to fetch the info  
-	//func called fetchWeather 
-	//we will make an api call by using the fetch
-	fetchWeather: function(city){ //when we search for city,the info would be stored
-		fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid="+this.apiKey)
-		.then((response)=>response.json()) //when we fetch the info then the response should be stored in the json format 
-		.then((data)=>console.log(this.displayWeather(data)));
-//use this whenever referring to diff objects in a class
+console.log("working");
+let weather={
+	apiKey:"f06efb9fc4cc400992a24f1a65a6aa02", //apikey 
+	/***********function to fetch and store the data**********/
+    fetchWeather:function(city){ 
+		//fetch the url 
+		fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+ "&units=metric&appid=" +
+        this.apiKey)
+    //asking for data and response  
+   .then((response) => {  //3.once we 'fetched' the api,we can make it do specific tasks using .then
+        if (!response.ok) {  //response is the func parameter taken..if response is not ok then,//response recieved in boolean true or false format
+          alert("No weather found."); //alert by showing a pop up box saying "weather not found"
+          //throw new Error("No weather found."); //new error object created which would say no weather found when 
+          //the response recieved is not true
+        }
+        return response.json(); //response is a constructor,return the response or all the info in the fetch url and store it in the json format
+      })
 
-	},  
-	//display the weather
-	displayWeather:function(data){ 
-		const {name}=data; 
-		const {icon,description}=data.weather[0]; 
-		const {temp,humidity}=data.main; 
-		const {speed}=data.wind; 
-		//const {whatever we want to make a variable,whose key values would change acc} = accessing the keys through the data 
-		//objects of the data stored in the variable format so that they can vary 
-		//logging the object turned variables accordingly 
-		console.log(name,icon,description,temp,humidity,speed);
-		document.querySelector(".city").innerText="Weather in" + name; 
-		document.querySelector(".icon").src="https://openweathermap.org/img/wn/"+icon+".png"
-		document.querySelector(".temp").innerText=temp+"degree celcius"; 
-		document.quesrySelector(".humidity").innerText="Humidity: " + humidity + "%"; 
-		document.querySelector(".wind").innerText="Wind speed: "+ speed+"km/hr";  
-		document.querySelector(".weather").classList.remove(".loading");
+	 
+	//
+	.then((data)=>this.displayWeather(data)); 
+} ,
+ /*********display the credentials on screen**********/
+ displayWeather: function(data){ 
+ 	//access diff elements 
+    const {name}=data; 
+     const { icon, description } = data.weather[0]; 
+  
+    const {temp,feels_like,temp_min,temp_max,humidity}=data.main; 
+    const {speed}=data.wind; 
+    //access and assign the inner texts accordingly 
+    document.querySelector(".city").innerText="Weather in "+ name;
+    document.querySelector(".description").innerText=description; 
+    document.querySelector(".humidity").innerText="Humidity: "+ humidity+"%"; 
+    document.querySelector(".wind").innerText="Windspeed: "+speed+" Km/hr"; 
+    //document.querySelector(".minTemp").innerText="Min Temp: "+ temp_min+" %"; 
+    //document.querySelector(".maxTemp").innerText="Max Temp: "+temp_max+" %"; 
+    //document.querySelector(".feels").innerText="Feels: "+ "°C";  
+    document.querySelector(".temp").innerText="Temperature: "+ temp+"°C";  
+    //remove the laoding part once all the info is obtained 
+    document.querySelector(".weather").classList.remove("loading"); 
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
 
-//class list=objects of declared class declared as elements,remove..rem,remove all features of loading as well
+ } ,
+ /**********connecting the search bar text and fetchWeather function******/
+ search: function(){
+    this.fetchWeather(document.querySelector(".search-bar"));
+ }
+ 
 
-	} 
-	search:function(){
- this.fetchWeather(document.querySelector(".searchbar").value );
-} 
+} ;  
+console.log(city);
 
-}; 
-//search key press listener 
-document.querySelector(".search button btn").addEventListener("click",function(){
-	weather.search(); // search function called
+ /*********reading the input in the search bar**********/ 
+ //through mouse click 
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();//search function should be implememted once i detect a click 
+
+
 }); 
-document.querySelector(".searchbar").addEventListener('keyup',function(event){ //listen to whenever the key is released
-	if (event.key=="Enter"){  //trace the events wherein the keyup wa for enter 
-		weather.search();
+//through the enter key
+document.querySelector(".search-bar").addEventListener("keyup", function (event) { 
+    if(event=="Enter"){
+  weather.search();//search function should be implememted once i detect a click 
+}
 
-	}
-})
+}); 
+/***********calling the function at last*******/
+weather.fetchWeather("Delhi");
